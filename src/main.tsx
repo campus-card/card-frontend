d388824ea5100d9ec87f8acd9dd4091016a706ea
wlow <2064954337@qq.com>
2024-08-27 23:40:49 +0800

    引入react-router以及redux-toolkit, 准备使用slice的写法; 复习react

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,10 @@ import App from '@/App.tsx'
 import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
+  // todo: 构建时移除StrictMode
   <StrictMode>
-    {/* CssBaseline - MUI中提供的类似normalize.css的浏览器样式重置 */}
+    {/* CssBaseline - MUI中提供的类似normalize.css的浏览器样式重置
+     相比于normalize.css, 能更适配MUI */}
     <CssBaseline />
     <App />
   </StrictMode>,
--- src/view/index/index.tsx
+++ src/view/index/index.tsx
@@ -1,12 +1,31 @@
 import '@/view/index/index.scss'
 import { Button } from '@mui/material'
+import { FC, useRef, useState } from 'react'
 
 export default function Index() {
+
+  let number = useRef<number>(1)
+  const test = () => {
+    number.current++
+    console.log('number = ', number.current)
+  }
+
+  const [num, setNum] = useState<number>(1)
+  const test1 = () => {
+    setNum(num => num + 1)
+    setNum(num => num + 1)
+  }
+
   return (
     <div className="index">
-      123
+      <Test />
       <h1>Index Page</h1>
-      <Button onClick={() => console.log('你好')} variant="contained">测试</Button>
+      <Button onClick={test} variant="contained">测试{number.current}</Button>
+      <Button onClick={test1} variant="contained">测试重新渲染{num}</Button>
     </div>
   )
 }
+
+const Test: FC = () => (
+  <div>123</div>
+)
